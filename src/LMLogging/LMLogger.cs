ac6d848d24b8bd2d4d7bd856923cf427a086f258
gusemery <nfemery@outlook.com>
2024-08-21 12:23:55 -0500

    Added iLogger example for implementation and dependency injection

---
+++ src/LMLogging/LMLogger.cs
@@ -0,0 +1,60 @@
+﻿using LogicMonitor.DataSDK.Api;
+using LogicMonitor.DataSDK;
+using Microsoft.Extensions.Logging;
+using LogicMonitor.DataSDK.Model;
+using System.Net;
+
+namespace LMLogging
+{
+    public sealed class LMLogger : ILogger
+    {
+        private string company = "";      //  Company name; or portal preves  <company>.logicmonitor.com
+        private string accessID = "";     //  LMv1 Access ID here
+        private string accessKey = "";    //  LMv1 Access Key here
+        private MyResponse responseInterface = new MyResponse();
+        private Configuration config;
+        Resource resource;
+        //Pass the Authenticate Variables as Enviroment variable.
+        private ApiClient apiClient;
+        private static Logs logs;
+        private LMLoggerConfiguration configuration;
+        public LMLogger()
+        {
+            try
+            {
+                configuration = new LMLoggerConfiguration();
+                var resourceName = Dns.GetHostName();
+                Dictionary<string, string> resourceIds = new Dictionary<string, string>();
+                resourceIds.Add("system.displayname", resourceName.ToString());
+
+                config = new Configuration(company: company, accessID: accessID, accessKey: accessKey);
+                apiClient = new ApiClient(config);
+                resource = new Resource(name: resourceName.ToString(), ids: resourceIds, create: true);
+                logs = new Logs(batch: false, interval: 0, responseCallback: responseInterface, apiClient: apiClient);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull => default!;
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return !configuration.LogLevels.ContainsKey(logLevel);
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            logs.SendLogs(state.ToString(), resource: resource);
+        }
+    }
+
+    public sealed class LMLoggerConfiguration
+    {
+        public Dictionary<LogLevel, bool> LogLevels { get;  set; } = new()
+        {
+            [LogLevel.Debug] = true
+        };
+    } 
+}
--- src/LMLogging/Program.cs
+++ src/LMLogging/Program.cs
@@ -14,46 +14,29 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Net;
 using RestSharp;
+using Microsoft.Extensions.Logging;
 
-namespace Example
+namespace LMLogging
 {
     class Program
     {
-
-        //  Please set the following ID's for your company, and portal.   Once the program is executed; the logging will be assigned to the computer that is running the logs.  That is set within the resourceName property below.
-
-
-        static string company = "";      //  Company name; or portal preves  <company>.logicmonitor.com
-        static string accessID = "";     //  LMv1 Access ID here
-        static string accessKey = "";    //  LMv1 Access Key here
-
-
         static void Main(string[] args)
         {
-            var resourceName = Dns.GetHostName();
             var temp = Process.GetCurrentProcess();
             var startTime = DateTime.UtcNow;
-            var startCpuUsage = Process.GetCurrentProcess().TotalProcessorTime;
+            var startCpuUsage = Process.GetCurrentProcess().PrivilegedProcessorTime;
             var endTime = DateTime.UtcNow;
             var endCpuUsage = Process.GetCurrentProcess().TotalProcessorTime;
             var cpuUsedMs = (endCpuUsage - startCpuUsage).TotalMilliseconds;
             var totalMsPassed = (endTime - startTime).TotalMilliseconds;
 
-            Dictionary<string, string> resourceIds = new Dictionary<string, string>();
-            resourceIds.Add("system.displayname", resourceName.ToString());
-            MyResponse responseInterface = new MyResponse();
-            Configuration config = new Configuration(company: company, accessID: accessID, accessKey: accessKey);
-            //Pass the Authenticate Variables as Enviroment variable.
-            ApiClient apiClient = new ApiClient(config);
-            Logs logs = new Logs(batch: false, interval: 0, responseCallback: responseInterface, apiClient: apiClient);
-
-            Resource resource = new Resource(name: resourceName.ToString(), ids: resourceIds, create: true);
+            ILogger logs = new LMLogger();
 
             while (true)
             {
-                string msg = "Program function  has CPU Usage " + (cpuUsedMs / (Environment.ProcessorCount * totalMsPassed)).ToString() + " Milliseconds";
+                string msg = $"Program function  has CPU Usage " + (cpuUsedMs / (Environment.ProcessorCount * totalMsPassed)).ToString() + " Milliseconds across (" + Environment.ProcessorCount+ ")";
 
-                logs.SendLogs(message: msg, resource: resource);
+                logs.Log(LogLevel.Warning, msg);
                 Thread.Sleep(15000);
             }
         }
